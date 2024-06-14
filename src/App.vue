751f6d7e0bde83327dfea92a702af9dd53c6a0cf
dlwjdals5600 <dlwjdals5600@github.com>
2024-06-14 19:41:41 +0900

    vue.js 3 Project Start

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
+
+  <div class="menu">
+    <a v-for="(menu_link,i) in menus" :key="i">{{menu_link}}</a>
+  </div>
+
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div v-for="(div_products,i) in products" :key="i">
+    <h4>{{div_products[0]}}</h4>
+    <p>{{div_products[1]}} 만원</p>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data(){
+    return{
+      products : {0:['역삼동원룸',60], 1:['천호동원룸',70], 2:['마포구원룸',80]},
+      menus : ['Home', 'Products', 'About'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +36,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
