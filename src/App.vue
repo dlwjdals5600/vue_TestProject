9b9fb48b58981e810489d5fd2ceec9ed1c292a71
dlwjdals5600 <dlwjdals5600@github.com>
2024-06-15 20:46:21 +0900

    Second vue Projects

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
 
+<div class="black-bg" v-if="modalOpen">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button @click="modalOpen = False">닫기</button>
+    </div>
+</div>
+
   <div class="menu">
     <a v-for="(menu_link,i) in menus" :key="i">{{menu_link}}</a>
   </div>
 
+
+
+
+
   <img alt="Vue logo" src="./assets/logo.png">
 
   <div v-for="(div_products,i) in products" :key="i">
-    <h4>{{div_products[0]}}</h4>
-    <p>{{div_products[1]}} 만원</p>
+    <img :src="getImageSrc(div_products[0])" class="poket-img">
+    <h4 @click="modalOpen = true">{{div_products[1]}}</h4>
+    <p>{{div_products[2]}} 만원</p>
+    <button @click="div_products[3]++">허위매물신고</button> <span>신고수 : {{div_products[3]}} </span>
   </div>
   
 </template>
@@ -20,16 +34,39 @@ export default {
   name: 'App',
   data(){
     return{
-      products : {0:['역삼동원룸',60], 1:['천호동원룸',70], 2:['마포구원룸',80]},
+      modalOpen : false,
+      products : {0:['./assets/paori.jpg','파오리',60,0], 1:['./assets/sunder.jpg','썬더',70,0], 2:['./assets/crap.jpg','크랩',80,0]},   // [원룸이름, 가격, 허위매물신고수]
       menus : ['Home', 'Products', 'About'],
     }
   },
+  methods :{
+    getImageSrc(imagePath){
+      return require(`${imagePath}`)
+    }
+  },
+
   components: {
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -46,5 +83,9 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+.poket-img {
+  width: 20%;
+  
 }
 </style>
